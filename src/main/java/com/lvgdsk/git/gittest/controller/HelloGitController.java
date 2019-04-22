3bd19418097d0301bcd3a8f2af09afba973240c6
lvgdsk <lvgdsk@163.com>
2019-04-23 01:27:14 +0800

    alter the function return of eat and play

--- src/main/java/com/lvgdsk/git/gittest/controller/HelloGitController.java
+++ src/main/java/com/lvgdsk/git/gittest/controller/HelloGitController.java
@@ -13,11 +13,11 @@ public class HelloGitController {
 
     @GetMapping("eat")
     public String eat(){
-        return "eat food";
+        return "eat fish";
     }
 
     @GetMapping("play")
     public String play(){
-        return "i am play basketball";
+        return "i am play badminton";
     }
 }
